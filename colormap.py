2e2789523bef661f6791e3cef9b9ef9a6a2b8d8d
nghi1810 <133078681+nghi1810@users.noreply.github.com>
2026-07-17 22:04:13 +0700

    Delete colormap.py

--- colormap.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-data = np.load(r"C:\Users\phand\Desktop\New folder\dataset\depth\apple_0.npy")
-
-plt.imshow(data, cmap="jet")
-plt.colorbar()
-plt.title("Depth range")
-plt.show()
